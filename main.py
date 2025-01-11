af366ec46d12f306ca3d2e5d53c3b748eaec96a4
notcelsius <149901647+notcelsius@users.noreply.github.com>
2025-01-10 17:41:50 -0800

    Changed name of app.py to main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, URL
+
+app = Flask(__name__)
